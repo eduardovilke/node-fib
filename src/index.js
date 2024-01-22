3de467b0c1b1390a7903b77085ca07c17eb5383f
Eduardo Vilke <eduardo.svilke@gmail.com>
2024-01-22 09:44:11 -0300

    feat: add routes fibonnaci and hello

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+function calculateFibonacci(num) {
+  if(num === 0 || num === 1) return num;
+
+  return calculateFibonacci(num - 1) + calculateFibonacci(num - 2);
+}
+
+app.get('/hello/:name', (req, res) => {
+  res.send(`<h1>Hello ${req.params.name}</h1>`)
+})
+
+app.get('/fibonacci/:number', (req, res) => {
+  const { number } = req.params;
+
+  res.send(`<h1>${calculateFibonacci(+number)}</h1>`)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
